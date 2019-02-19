5eb703ddf64df74645d56bc407dcc7998f8a5532
Stan Graafmans <stan_live@hotmail.nl>
2019-02-19 11:23:44 +0100

    Organized utils
    Signed-off-by: Stan Graafmans <stan_live@hotmail.nl>

---
+++ TowerDefense_Unity/Assets/Scripts/Utils/SceneUtility.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.Utils
+{
+    public class SceneUtility : MonoBehaviour
+    {
+#if UNITY_EDITOR || DEBUG_ENABLED
+        [SerializeField] private KeyCode m_ReloadKey = KeyCode.F1;
+        [SerializeField] private KeyCode m_NextKey = KeyCode.F2;
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(m_ReloadKey))
+            {
+                ReloadActive();
+            }
+
+            if (Input.GetKeyUp(m_NextKey))
+            {
+                LoadNext();
+            }
+        }
+#endif
+
+        public static void ReloadActive()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public static void LoadPrevious()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+
+        public static void LoadNext()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
