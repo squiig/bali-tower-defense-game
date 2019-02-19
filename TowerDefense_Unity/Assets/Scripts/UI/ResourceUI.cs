972d3fcec1bc72df5ddd99936d8b21be38dc33fa
Stan Graafmans <stan_live@hotmail.nl>
2019-02-19 13:23:20 +0100

    Finished resource hud for now
    Signed-off-by: Stan Graafmans <stan_live@hotmail.nl>

---
+++ TowerDefense_Unity/Assets/Scripts/UI/ResourceUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Game.UI
+{
+    public class ResourceUI : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _AmountText = null;
+
+        private int _Amount = 0;
+
+        public int Amount => _Amount;
+
+        private void OnEnable()
+        {
+            // subscribe AmountChanged to backend here
+        }
+
+        private void OnDisable()
+        {
+            // unsubscribe AmountChanged from backend here
+        }
+
+        private void AmountChanged(int amount)
+        {
+            _Amount = amount;
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            _AmountText.text = _Amount.ToString("D4");
+        }
+    }
+}
