8ceb4a652b6e805f4a9d5145c46238fc7ab63227
Tom Lemmers <xizzex@outlook.com>
2019-02-21 13:19:29 +0100

    Added the basic folder/script structure

---
+++ TowerDefense_Unity/Assets/Editor/BackEnd/TurretGrid/TurretGridEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Game.BackEnd.Turrets.Editor
+{
+    /// <summary>Draws the grid in the scene view.</summary>
+    public class TurretGridEditor : UnityEditor.Editor
+    {
+        private SerializedObject _SerializedTurretGrid;
+
+        public void OnEnable()
+        {
+            
+        }
+
+        public void OnDisable()
+        {
+            
+        }
+
+        private void OnSceneGUI()
+        {
+
+        }
+    }
+}
---
+++ TowerDefense_Unity/Assets/Resources/BackEnd/TurretGrid/TurretGrid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.BackEnd.Turrets
+{
+    /// <summary>Contains data of the gridcell itself.</summary>
+    public struct TurretGridCell
+    {
+        public bool _IsOccupied { get; set; }
+    }
+
+    /// <summary>Holds the data of the turretgrid.</summary>
+    public class TurretGrid : MonoBehaviour
+    {
+        [SerializeField] private Vector2 _CellSize, _CellResolution;
+        private TurretGridCell[] _TurretGrid;
+
+        private void Awake()
+        {
+            
+        }
+    }
+}
