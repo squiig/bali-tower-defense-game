93d7a32842c6e77b5635b400f981e27f324b2844
Tom Lemmers <xizzex@outlook.com>
2019-02-27 01:45:35 +0100

    Merge branch 'develop' into feature/environmental_spline_system
    # Conflicts:
    #	TowerDefense_Unity/Assets/Editor/BackEnd/SplineSystem/SplineEditor.cs
    #	TowerDefense_Unity/Assets/Scenes/Tom_TurretGrid.unity
    #	TowerDefense_Unity/Assets/Scripts/BackEnd/SplineSystem/BezierSplineDataObject.cs
    #	TowerDefense_Unity/Assets/Scripts/BackEnd/SplineSystem/EvenlySpacedPoints.cs
    #	TowerDefense_Unity/Assets/Scripts/BackEnd/SplineSystem/SplineCreator.cs

---
+++ TowerDefense_Unity/Assets/Scripts/Entities/Interfaces/IPlaceableEntity.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game.Entities.Interfaces
+{ 
+	/// <summary>
+	/// Interface representing entities that can be placed.
+	/// </summary>
+	public interface IPlaceableEntity
+	{
+		/// <summary>
+		/// Used to construct this instance at given
+		/// position and rotation. This entity will regulate
+		/// itself after spawning.
+		/// </summary>
+		/// <param name="position"> Position to place this entity upon.</param>
+		/// <param name="rotation"> Rotation to give to this entity. </param>
+		void Construct(Vector3 position, Vector3 rotation);
+
+		/// <summary>
+		/// Used to destroy this entity.
+		/// Ignores health(if applicable) and destroys the entity.
+		/// </summary>
+		void Destruct();
+	}
+}
---
+++ TowerDefense_Unity/Assets/Scripts/Entities/Interfaces/IUpgradeable.cs
@@ -0,0 +1,28 @@
+﻿namespace Game.Entities.Interfaces
+{
+	/// <summary>
+	/// Interface representing that the inheritor can be upgraded by param T.
+	/// </summary>
+	/// <typeparam name="TUpgrade"> The upgrade that can be applied to the inheritor.</typeparam>
+	/// <typeparam name="TUpgradeDependency">Class that has to be upgraded.</typeparam>
+	public interface IUpgradeable<TUpgrade, TUpgradeDependency> 
+		where TUpgrade : IUpgrade<TUpgradeDependency> where TUpgradeDependency : class
+	{
+		/// <summary>
+		/// Upgrades the instance by T.
+		/// T must be gotten via <see cref="GetPossibleUpgrades"/>
+		/// Upgrade can be rejected if not enough resources are available.
+		/// </summary>
+		/// <param name="upgrade"> Upgrade to apply to this instance.</param>
+		void Upgrade(TUpgrade upgrade);
+
+		/// <summary>
+		/// Returns an array of T containing all
+		/// upgrades of T. If an upgrade is applied it will
+		/// be removed from this list.
+		/// Will never be null.
+		/// </summary>
+		/// <returns> An array of T with all possible upgrades. Never null.</returns>
+		void GetPossibleUpgrades(out TUpgrade[] upgrades);
+	}
+}
