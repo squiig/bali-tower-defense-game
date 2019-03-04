0ee960dd1261f5e14bfa6ec3c5c24a601bdf63dc
Boudewijn Witteveen <516708@student.glu.nl>
2019-03-04 14:03:52 +0100

    Added basic healthbar and made the fuctions that have to subscribe to the enemy to gain the health values

--- TowerDefense_Unity/Assets/Scripts/UI/EnemyUi.cs
+++ TowerDefense_Unity/Assets/Scripts/UI/EnemyUi.cs
@@ -1,11 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.UI
 {
-    public class EnemyUi : MonoBehaviour
+    public class EnemyUI : MonoBehaviour
     {
+        [SerializeField] private Image _HealthBarHealthLayer;
+        [SerializeField] private Image _HealthBarDamageLayer;
 
+        [SerializeField] private float _CurrentHealth;
+        [SerializeField] private float _MaxHealth;
+
+        private void Start()
+        {
+            if(_HealthBarDamageLayer == null || _HealthBarDamageLayer == null)
+            {
+                if (_HealthBarDamageLayer == null)
+                    Debug.LogError("Please select the Health Bar Damage Layer in the inspector.", this.gameObject);
+                if(_HealthBarHealthLayer == null)
+                    Debug.LogError("Please select the Health Bar Layer in the inspector.", this.gameObject);
+
+                this.enabled = false;
+                return;
+            }
+
+            _HealthBarHealthLayer.fillAmount = 1;
+            _HealthBarDamageLayer.fillAmount = 0;
+        }
+
+        public void SetStartHealth(float health)
+        {
+            //TODO: Subscribe to the enemy script to know when the damage is done
+            
+            _CurrentHealth = health;
+            _MaxHealth = health;
+        }
+        public void SetHealthDamage(float health)
+        {
+            _CurrentHealth -= health;
+
+            StartCoroutine(UpdateHealthBar());
+        }
+
+        private IEnumerator UpdateHealthBar()
+        {
+            float healthPercentage = _CurrentHealth / 100;
+
+            Debug.Log(_HealthBarHealthLayer.fillAmount);
+            Debug.Log(healthPercentage);
+
+            while(_HealthBarHealthLayer.fillAmount >= healthPercentage)
+            {
+                _HealthBarHealthLayer.fillAmount -= 0.01f;
+
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                SetHealthDamage(10f);
+            }
+        }
     }
 }
