ebfde1fd21502763d42dc64a51acca5d2ed46a02
Stan Graafmans <stan@stangraafmans.com>
2019-03-05 14:48:15 +0100

    Separated resource ui scripts for main and normal

--- TowerDefense_Unity/Assets/Scripts/UI/ResourceUI.cs
+++ TowerDefense_Unity/Assets/Scripts/UI/ResourceUI.cs
@@ -5,34 +5,15 @@ using TMPro;
 
 namespace Game.UI
 {
-    /// <summary>
-    /// This script makes sure the corresponding resource value in the backend gets properly displayed in the UI elements.
-    /// </summary>
+	/// <summary>
+	/// This component allows for dynamically displaying an amount in UI text, with customizable zero padding.
+	/// </summary>
     public class ResourceUI : MonoBehaviour
     {
         private const string ZERO_PADDING_TOOLTIP = "How many digits are used for the amount, where any leftover digits will be zeroes (e.g. 00042 is a padding of five).";
 
         [SerializeField] private TMP_Text _AmountText = null;
         [SerializeField, Tooltip(ZERO_PADDING_TOOLTIP)] private int _ZeroPadding = 5;
-
-        private void OnEnable()
-        {
-            //TODO: subscribe AmountChanged to backend here
-        }
-
-        private void OnDisable()
-        {
-            //TODO: unsubscribe AmountChanged from backend here
-        }
-
-        /// <summary>
-        /// Event hook to be called when the resource value changes in backend.
-        /// </summary>
-        /// <param name="amount"></param>
-        private void AmountChanged(int amount)
-        {
-            SetAmount(amount);
-        }
 
 		/// <summary>
 		/// Sets the visual number to the desired amount.
