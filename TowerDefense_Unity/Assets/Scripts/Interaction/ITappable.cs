d5112c26d4ca4be8f6bffbe39c598095bf44cb18
Errorcodebin <errorcodebin_2@hotmail.com>
2019-03-19 13:28:13 +0100

    Fixed ITappable summary

--- TowerDefense_Unity/Assets/Scripts/Interaction/ITappable.cs
+++ TowerDefense_Unity/Assets/Scripts/Interaction/ITappable.cs
@@ -1,8 +1,10 @@
+using UnityEngine;
+
 namespace Game.Interaction
 {
 	/// <summary>
 	/// Interface to recieve tap events on a monobehaviour.
-	/// NOTE: Needs a collider on the same object or parent object to work.
+	/// NOTE: Needs a collider on the gameobject or parent object to work
 	/// </summary>
 	public interface ITappable
 	{
