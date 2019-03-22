509a1abb4fb4856c089e2090d58fd47209c791b0
Errorcodebin <errorcodebin_2@hotmail.com>
2019-03-22 08:16:27 +0100

    Added Towerselection to keep track of which tower we're supposed to build

---
+++ TowerDefense_Unity/Assets/Scripts/Interaction/TowerSelection.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace Game.Interaction
+{
+	/// <summary>
+	/// Keeps track of which tower is selected inside of the tower gui. 
+	/// </summary>
+	public class TowerBuildSelection : DDOLMonoBehaviourSingleton<TowerBuildSelection>
+	{
+		private GameObject _SelectedObject;
+		public GameObject selectedObject { get; set; }
+		public bool hasSelectedObject { get => _SelectedObject != null; }
+
+		public event System.Action<GameObject> OnSelectionRecieved;
+		public event System.Action<GameObject> OnSelectionCleared;
+
+		/// <summary>
+		/// Clears the selection.
+		/// </summary>
+		public void Clear()
+		{
+			OnSelectionCleared?.Invoke(_SelectedObject);
+			_SelectedObject = null;
+		}
+
+		/// <summary>
+		/// Returns the currently selected tower and clears the selection.
+		/// </summary>
+		public GameObject Take(GameObject tower)
+		{
+			GameObject selected = _SelectedObject;
+			Clear();
+			return selected;
+		}
+
+		/// <summary>
+		/// Sets the current selected tower tto build
+		/// </summary>
+		public void SetTowerSelected(GameObject tower)
+		{
+			_SelectedObject = tower;
+			OnSelectionRecieved?.Invoke(tower);
+		}
+	}
+}
+
