911c234d9c40e9573f6a2cfcb381aa5c26f4f6b0
Sivves <stan_live@hotmail.nl>
2019-03-29 15:53:51 +0100

    Update TowerDefense_Unity/Assets/Scripts/UI/Menu/MainMenuUI.cs

--- TowerDefense_Unity/Assets/Scripts/UI/Menu/MainMenuUI.cs
+++ TowerDefense_Unity/Assets/Scripts/UI/Menu/MainMenuUI.cs
@@ -10,7 +10,7 @@ namespace Game.UI
 		private void Update()
 		{
 			//If someone either touches the screen or pressed the left mouse button, go the next scene
-			if (Input.GetMouseButtonDown(0))
+			if (Input.GetMouseButtonUp(0))
 				//TODO: Ones there is a new Scene Loader, replace this line with that.
 				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		}
