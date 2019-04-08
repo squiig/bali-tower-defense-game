461c9367a4dfc264d489c9c51abf43eb0cc734e1
Coda <Errorcodebin_2@hotmail.com>
2019-04-08 14:30:22 +0200

    Fixed minions dying at start and legion not being recognized by Unity
    BLah

---
+++ TowerDefense_Unity/Assets/Scripts/WaveSystem/WaveLegion.cs
@@ -0,0 +1,16 @@
+﻿using Game.Entities.MovingEntities;
+using UnityEngine;
+
+namespace Game.WaveSystem
+{
+	[CreateAssetMenu(fileName = "NewLegion", menuName = "Wave/Legion")]
+	public class WaveLegion : ScriptableObject
+	{
+		[SerializeField] private Minion[] _Minions;
+		[SerializeField] float _SpawnInterval;
+
+		public Minion this[int i] => _Minions[i];
+		public int MinionCount => _Minions.Length;
+		public float SpawnInterval => _SpawnInterval;
+	}
+}
