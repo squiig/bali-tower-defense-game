5bf85700eeb9f1b5577f9638a3db43f09c5b56f2
Coda <Errorcodebin_2@hotmail.com>
2019-04-23 12:42:51 +0200

    Fixed build error

--- TowerDefense_Unity/Assets/HolyShift/Scripts/CheckForStandardAssets.cs
+++ TowerDefense_Unity/Assets/HolyShift/Scripts/CheckForStandardAssets.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -8,8 +8,10 @@ public class CheckForStandardAssets : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
-	    var guids = AssetDatabase.FindAssets("FXWater4Advanced", null);
+#if UNITY_EDITOR
+		var guids = AssetDatabase.FindAssets("FXWater4Advanced", null);
 	    Debug.Assert(guids.Length > 0, "Please add Unity's Standard Assets to make water works! https://www.assetstore.unity3d.com/en/#!/content/32351");
+#endif
 	}
 	
 	// Update is called once per frame
